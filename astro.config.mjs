595205f4937e66ab6ba736588a0cece57a8cf1d5
Arthur Piloto <arthurpiloto.github@gmail.com>
2026-03-20 14:42:06 -0300

    configuration of build instruction

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import relativeLinks from "astro-relative-links";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+  },
+  integrations: [relativeLinks()],
+  build: {
+    assets: "assets",
+    format: "file",
   },
 });
